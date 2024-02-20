7b974fe8fe22f74f650052c7dc38d1f0335cba76
Mrutunjay113 <mrutunjay765@gmail.com>
2024-02-20 10:46:51 +0530

    Add user page layout and styling,pagination component

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,3 +1,62 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/user.module.css";
+import Image from "next/image";
+import Link from "next/link";
 export default function page() {
-  return <main>User page</main>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add User</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Created At</th>
+            <th>Role</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="user"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>13.01.2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.delete} ${styles.button}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </main>
+  );
 }
