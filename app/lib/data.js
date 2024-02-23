ce7c406c1601409193de2bfe05b14e8620b19e39
Mrutunjay113 <mrutunjay765@gmail.com>
2024-02-24 00:14:56 +0530

    add search user

--- app/lib/data.js
+++ app/lib/data.js
@@ -20,9 +20,10 @@ import ConnectMongoDb from "./utils";
 // };
 
 ConnectMongoDb();
-export const fetchUsers = async () => {
+export const fetchUsers = async (q) => {
+  const regex = new RegExp(q, "i");
   try {
-    const users = await User.find();
+    const users = await User.find({ username: { $regex: regex } });
     return users;
   } catch (err) {
     console.log(err);
--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,12 +1,39 @@
+"use client";
 import React from "react";
 import styles from "./search.module.css";
 import { MdSearch } from "react-icons/md";
+import {
+  useParams,
+  useSearchParams,
+  useRouter,
+  usePathname,
+} from "next/navigation";
 
 const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = (e) => {
+    const search = e.target.value;
+    const params = new URLSearchParams(searchParams);
+    if (search) {
+      search.length > 2 && params.set("q", search);
+      replace(`${pathname}?${params.toString()}`);
+    } else {
+      params.delete("q");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
   return (
     <div className={styles.container}>
       <MdSearch />
-      <input type="search" placeholder={placeholder} className={styles.input} />
+      <input
+        type="search"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={handleSearch}
+      />
     </div>
   );
 };
